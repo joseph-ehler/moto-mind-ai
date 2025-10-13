1d0a855be3c5b8c8c4686c5e658119a59d43258b
Joseph Ehler <josephehler@Josephs-MacBook-Pro-2.local>
2025-10-13 15:11:54 -0400

    PWA complete: install prompt banner + manifest on all pages

--- app/client-providers.tsx
+++ app/client-providers.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from 'react'
 import { SessionProvider } from '@/components/providers/SessionProvider'
 import { ToastProvider } from '@/components/design-system/feedback/ToastNotifications'
+import { PWAInstallPrompt, OfflineIndicator } from '@/components/PWAInstallPrompt'
 import { registerServiceWorker } from '@/lib/pwa/register-sw'
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
@@ -17,6 +18,8 @@ export function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <ToastProvider position="top-right">
+        <PWAInstallPrompt />
+        <OfflineIndicator />
         {children}
       </ToastProvider>
     </SessionProvider>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app'
 import { PWAManifestInjector } from '@/components/PWAManifestInjector'
+import { PWAInstallPrompt, OfflineIndicator } from '@/components/PWAInstallPrompt'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <PWAManifestInjector />
+      <PWAInstallPrompt />
+      <OfflineIndicator />
       <Component {...pageProps} />
     </>
   )
