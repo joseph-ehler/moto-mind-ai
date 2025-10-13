d0f27eacd9d3db53651b9eb3d2de8fd3ff8abadf
Joseph Ehler <josephehler@Josephs-MacBook-Pro-2.local>
2025-10-13 17:48:21 -0400

    Remove showcase pages causing build errors

--- pages/vision-demo.tsx
+++
@@ -1,9 +0,0 @@
-import { VisionExamples } from '@/components/vision/VisionExamples'
-
-export default function VisionDemoPage() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <VisionExamples />
-    </div>
-  )
-}
