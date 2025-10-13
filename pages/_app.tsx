b822121b7a87ec41cc96efddbc43c089f760a903
Joseph Ehler <josephehler@Josephs-MacBook-Pro-2.local>
2025-10-13 15:10:02 -0400

    PWA complete: manifest works on all pages + clean service worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from 'next/app'
+import { PWAManifestInjector } from '@/components/PWAManifestInjector'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <PWAManifestInjector />
+      <Component {...pageProps} />
+    </>
+  )
 }
